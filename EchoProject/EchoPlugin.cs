9041404da6e32f63cd3057f2f5e797619302dfd8
vibeside <vibeside01@gmail.com>
2024-02-01 16:41:33 -0500

    first

---
+++ EchoProject/EchoPlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BepInEx;
+namespace EchoProject
+{
+    [BepInPlugin(modGuid,modName,"1.0.0.0")]
+    public class EchoPlugin
+    {
+        public const string modGuid = "grug.lethalcompany.EchoSense";
+        public const string modName = "EchoSense";
+    }
+}
