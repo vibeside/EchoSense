634337c42eeeb5514fa9ef3345b83577e44e7e42
vibeside <vibeside01@gmail.com>
2024-02-01 22:23:17 -0500

    embedded a bundle, and added a few lines and a dependency

--- EchoProject/EchoPlugin.cs
+++ EchoProject/EchoPlugin.cs
@@ -2,20 +2,22 @@
 using System.IO;
 using System.Reflection;
 using BepInEx;
+using CustomPostProcessingAPI;
 using UnityEngine;
 namespace EchoProject
 {
     [BepInPlugin(modGuid,modName,"1.0.0.0")]
+    [Bep]
     public class EchoPlugin
     {
         public const string modGuid = "grug.lethalcompany.EchoSense";
         public const string modName = "EchoSense";
-        public static AssetBundle bundle;
+        public static AssetBundle assetBundle = AssetBundle.LoadFromMemory(Properties.Resources.sonarbundle);
+        public static Material assetMat = assetBundle.LoadAsset<Material>("Assets/fulscreen stuff/FS_Material.mat");
         public void Awake()
         {
-            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-            bundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "sonarBundle"));
+            PostProcess sonar = new PostProcess("Sonar", assetMat);
+            CustomPostProcessingManager.Instance.AddPostProcess(sonar);
         }
     }
 }
